feb32571728400fc68878c6b46f7a141a2aab9e5
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-04-15 15:47:51 +0000

    Add detailview to goals

--- base/urls.py
+++ base/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import GoalsList
+from .views import GoalsList, GoalsDetail
 
 urlpatterns = [
     path('', GoalsList.as_view(), name= 'goals'),
+    path('goal/<int:pk>/', GoalsDetail.as_view(), name='goal')
 ]
--- base/views.py
+++ base/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 from .models import Goal
 
 # Create your views here.
 class GoalsList(ListView):
     model = Goal
-        
+    context_object_name = 'goals'
+
+class GoalsDetail(DetailView):
+    model = Goal
+    context_object_name = 'goal'
+    template_name = 'base/goal.html'
