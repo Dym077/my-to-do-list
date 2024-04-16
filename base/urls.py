aeb0bc876f8cfb067127a929afd1ad13810240f4
Andreas Sandberg <andreas.sandberg1.as@gmail.com>
2024-04-16 18:45:03 +0000

    Create user logout

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import GoalsList, GoalsDetail, GoalCreate, GoalUpdate, DeleteView, UserLoginView
-
+from django.contrib.auth.views import LogoutView
 urlpatterns = [
     path('login/', UserLoginView.as_view(), name='login'), 
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('', GoalsList.as_view(), name='goals'),
     path('goal/<int:pk>/', GoalsDetail.as_view(), name='goal'),
     path('goal-create/', GoalCreate.as_view(), name= 'goal-create'),
